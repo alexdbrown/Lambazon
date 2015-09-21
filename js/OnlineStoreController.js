3c9d631d7a088c5ed6e80c2329b53b7e2610c8b0
Alexandra Brown & Phillip Shannon <student@epicodus.com>
2015-09-21 15:13:24 -0700

    write html allowing ability toadd a product

--- js/OnlineStoreController.js
+++ js/OnlineStoreController.js
@@ -1,1 +1,11 @@
-
+onlineStore.controller('ProductsCtrl', function ProductsCtrl($scope) {
+  $scope.products = [];
+  $scope.addProduct = function() {
+    $scope.products.push({
+      name: $scope.productName,
+      description: $scope.productDescription,
+      price: $scope.productPrice,
+      quantity: $scope.productQuantity
+    });
+  }
+});
